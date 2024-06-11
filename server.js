fe67b3e87c43fcd044d9fc05c596debc0cba729c
Pranav Lonari <pranavlonari12@gmail.com>
2024-06-11 16:20:58 +0530

    added mongodb connection

--- server.js
+++ server.js
@@ -7,7 +7,22 @@ const mongoose = require("mongoose");
 
 const app = express();
 const server_config = require("./configs/server.config");
+const db_config = require("./configs/db.config");
+/**
+ * create an admin user at the starting of the application
+ * if not already present
+ */
 
+//connection with mongodb
+mongoose.connect(db_config.DB_URL);
+
+const db = mongoose.connection;
+db.on("error", () => {
+  console.log("error while connecting");
+});
+db.on("open", () => {
+  console.log("Connected to Mongodb");
+});
 /**
  * Start the server
  */
