4dcf3227336199500cb2ead37c078be37af05c2d
Pranav Lonari <pranavlonari12@gmail.com>
2024-06-12 12:42:15 +0530

    added middleware

---
+++ middlewares/auth.mw.js
@@ -0,0 +1,50 @@
+/**
+ * create a mw will check if the request body is proper and correct
+ *
+ */
+
+const user_model = require("../models/user.model");
+
+const verifySignUpBody = async (req, res, next) => {
+  try {
+    // Check name
+    if (!req.body.name) {
+      return res.status(400).send({
+        message: "failed! Name was not provided in request body",
+      });
+    }
+
+    // Check email
+    if (!req.body.email) {
+      return res.status(400).send({
+        message: "failed! Email was not provided in request body",
+      });
+    }
+
+    // Check userId
+    if (!req.body.userId) {
+      return res.status(400).send({
+        message: "failed! UserId was not provided in request body",
+      });
+    }
+
+    // Check if the user with the same userId is already present
+    const user = await user_model.findOne({ userId: req.body.userId });
+
+    if (user) {
+      return res.status(400).send({
+        message: "failed! User with the same userId is already present",
+      });
+    }
+    next();
+  } catch (err) {
+    console.log("Error while validating");
+    res.status(500).send({
+      message: "Error while validating",
+    });
+  }
+};
+
+module.exports = {
+  verifySignUpBody: verifySignUpBody,
+};
